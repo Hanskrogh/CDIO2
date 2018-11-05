8d91a8724868a8e264ff008ae76335e86126321c
Jeppe <jeppe@kaarelarsen.dk>
2018-11-05 10:58:10 +0100

    Har tilføjet en simpel view-klasse, som skal implementeres iht. MVC.

---
+++ src/main/java/spil/View.java
@@ -0,0 +1,20 @@
+package spil;
+
+/**
+ * <p>
+ * The View-class is an important part of the MVC-architecture.
+ * It allows the programmer to change the user interface at any time with no need to change any other classes.
+ * </p>
+ */
+public class View {
+
+    /**
+     * Prints a message to the user.
+     * @param message Description The string to be printed to the user.
+     */
+
+    public static void print(String message){
+        System.out.println(message);
+    }
+
+}
