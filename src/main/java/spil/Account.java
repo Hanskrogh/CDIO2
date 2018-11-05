206c69e0c480d5df92b410219dd131599f8ba8d4
Nicklas185105 <s185105@student.dtu.dk>
2018-11-05 10:29:33 +0100

    Nicklas - Lavet basic layout til javaDoc, sådan så den er klar til at få skrevet dokumentation

--- src/main/java/spil/Account.java
+++ src/main/java/spil/Account.java
@@ -1,5 +1,9 @@
 package spil;
 
+/**
+ *
+ * @since 1.0.1
+ */
 public class Account {
 
 
--- src/main/java/spil/Dice.java
+++ src/main/java/spil/Dice.java
@@ -2,22 +2,39 @@ package spil;
 
 import java.util.Random;
 
+/**
+ *
+ * @since 1.0.1
+ */
 public class Dice {
     Random random;
     int vaule = 1;
 
     public final static int DiceSides = 6;
 
+    /**
+     *
+     * @since 1.0.1
+     */
     public Dice() {
         this.random = new Random();
     }
 
-    // Retunerer en værdi mellem 1-TerningSides
+    /**
+     *
+     * @return the value between 1 dice side.
+     * @since 1.0.1
+     */
     public int cast() {
         random = new Random(random.nextInt());//tving ny random per kald
         vaule = random.nextInt(DiceSides)+1;
         return vaule;
     }
 
+    /**
+     *
+     * @return Returning the value of the dice side.
+     * @since 1.0.1
+     */
     public int getVaule() { return vaule; }
 }
