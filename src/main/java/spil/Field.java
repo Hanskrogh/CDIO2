3c593bceca64443d873af5cf2b0732e075b9fe4a
Rasmus <rsoeborg@gmail.com>
2018-11-05 13:08:48 +0100

    Lavet java "Field" class

--- src/main/java/spil/Field.java
+++ src/main/java/spil/Field.java
@@ -5,6 +5,23 @@ package spil;
  * @since 1.0.1
  */
 public class Field {
-    private String name;
+    public final String name;
+    public final int value;
+    public final boolean getsAnotherTurn;
+    public final String fieldText;
 
+    Field(String name, int value, String fieldText) {
+        this.name = name;
+        this.value = value;
+        this.fieldText = fieldText;
+        getsAnotherTurn = false;
+    }
+
+    Field(String name, int value, String fieldText, boolean getsAnotherTurn) {
+        this.name = name;
+        this.value = value;
+        this.fieldText = fieldText;
+        this.getsAnotherTurn = getsAnotherTurn;
+    }
 }
+
